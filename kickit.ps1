936f418653a7fe7837b645ccd905dd1d31ebf928
Brodski <cbrodski@gmail.com>
2021-11-17 19:12:54 -0700

    update

--- kickit.ps1
+++ kickit.ps1
@@ -1,1 +1,10 @@
-mvn spring-boot:run -Dprofiles=dev
+start mongodb
+ - windows: B:\Program Files\MongoDB\Server\3.4\bin\mongod.exe?
+
+GO INTO application.properties AND CHANGE THIS
+  spring.profiles.active=dev
+
+$ mvn spring-boot:run 
+
+
+#### mvn package    
