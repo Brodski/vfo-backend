0ae0b857728f7231a3213c41620fd00bb6d05c35
Brodski <cbrodski@gmail.com>
2021-11-20 15:20:00 -0700

    comments

--- kickit.ps1
+++ kickit.ps1
@@ -4,7 +4,9 @@ start mongodb
 GO INTO application.properties AND CHANGE THIS
   spring.profiles.active=dev
 #Prod
+# nohup command >/dev/null 2>&1 &
 $ sudo mvn package && sudo nohup java -jar target/restApi-0.0.1-SNAPSHOT.jar -Dprofiles.active=prod &
+$ sudo mvn package && nohup nohup java -jar target/restApi-0.0.1-SNAPSHOT.jar -Dprofiles.active=prod  > /dev/null 2>&1 & 
 # without nohup
 $ sudo mvn package && sudo java -jar target/restApi-0.0.1-SNAPSHOT.jar -Dprofiles.active=prod 
 
